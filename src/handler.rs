dd1e70fb00730d1027203799548911ec7a366820
Maxime Mouchet <max@maxmouchet.com>
2025-01-12 17:58:38 +0100

    upgrade to caracat v1.3.0

--- src/handler.rs
+++ src/handler.rs
@@ -1,27 +1,61 @@
 use anyhow::Result;
-use caracat::high_level::Config;
 use caracat::models::Probe;
+use caracat::rate_limiter::RateLimitingMethod;
 use log::{info, warn};
 use rdkafka::message::BorrowedMessage;
 use rdkafka::message::{Headers, Message};
 use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::Ipv6Addr;
 use std::time::Duration;
 use tokio::task;
 
 use crate::prober::probe;
 
+/// Probing configuration.
+#[derive(Debug)]
+pub struct Config {
+    /// Number of probes to send before calling the rate limiter.
+    pub batch_size: u64,
+    /// Identifier encoded in the probes (random by default).
+    pub instance_id: u16,
+    /// Whether to actually send the probes on the network or not.
+    pub dry_run: bool,
+    /// Do not send probes with ttl < min_ttl.
+    pub min_ttl: Option<u8>,
+    /// Do not send probes with ttl > max_ttl.
+    pub max_ttl: Option<u8>,
+    /// Check that replies match valid probes.
+    pub integrity_check: bool,
+    /// Interface from which to send the packets.
+    pub interface: String,
+    /// Source IPv4 address
+    pub src_ipv4_addr: Option<Ipv4Addr>,
+    /// Source IPv6 address
+    pub src_ipv6_addr: Option<Ipv6Addr>,
+    /// Maximum number of probes to send (unlimited by default).
+    pub max_probes: Option<u64>,
+    /// Number of packets to send per probe.
+    pub packets: u64,
+    /// Probing rate in packets per second.
+    pub probing_rate: u64,
+    /// Method to use to limit the packets rate.
+    pub rate_limiting_method: RateLimitingMethod,
+    /// Time in seconds to wait after sending the probes to stop the receiver.
+    pub receiver_wait_time: Duration,
+}
+
 fn create_config() -> Config {
     Config {
-        allowed_prefixes_file: None,
-        blocked_prefixes_file: None,
         batch_size: 128,
         dry_run: false,
-        extra_string: None,
         min_ttl: None,
         max_ttl: None,
         integrity_check: true,
         instance_id: 0,
         interface: caracat::utilities::get_default_interface(),
+        src_ipv4_addr: None,
+        src_ipv6_addr: None,
         max_probes: None,
         packets: 1,
         probing_rate: 100,
